ac1d766b682e53c911ebf15c4ed43017869f84a7
vikrams169 <vikramsetty169@gmail.com>
2024-03-01 19:44:01 -0500

    The code is now working. Need to add functionality to display the frames and generate the video

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ frame_number = 0
 def save_frame(viz_window):
     global frame_number
     pygame.display.update()
-    pygame.image.save(viz_window,FRAME_DIRECTORY+"/frame"+str(frame_number)+".jpg")
-    frame_number += 1
+    #pygame.image.save(viz_window,FRAME_DIRECTORY+"/frame"+str(frame_number)+".jpg")
+    #frame_number += 1
 
 def initialize_map(viz_window):
     wall1_bloated = [(0,0),(5,0),(5,500),(0,500)]
@@ -71,16 +71,40 @@ def reached_goal(loc):
         return True
     else:
         return False
+    
+def compute_final_path(viz_window):
+    path_nodes = [goal_pos]
+    while not np.array_equal(path_nodes[-1],start_pos):
+        parent_loc = path_nodes[-1]
+        path_nodes.append((int(node_info[parent_loc[0],parent_loc[1],1]),int(node_info[parent_loc[0],parent_loc[1],2])))
+    for loc in path_nodes:
+        pygame.draw.rect(viz_window,RED,(loc[0],loc[1],WIDTH_X,WIDTH_Y))
+    save_frame(viz_window)
 
-def add_node(viz_window,parent_node,new_loc):
-    global node_info
-    pass
+def add_node_to_heap(viz_window,parent_loc,new_loc,op):
+    global min_heap
+    if new_loc[0] < 0 or new_loc[0] >= GRID_SIZE[0] or new_loc[1] < 0 or new_loc[1] >= GRID_SIZE[1] or in_obstacle(viz_window,(new_loc[0],new_loc[1])) or node_info[new_loc[0],new_loc[1],0] == 1:
+        return
+    dist = None
+    if op == 0:
+        dist = node_info[parent_loc[0],parent_loc[1],3] + 1
+    else:
+        dist = node_info[parent_loc[0],parent_loc[1],3] + 1.4
+    heappush(min_heap,(dist,new_loc,parent_loc))
+    pygame.draw.rect(viz_window,GREEN,(new_loc[0],new_loc[1],WIDTH_X,WIDTH_Y))
 
-def add_neighbors(viz_window_node):
-    global node_info
+def add_neighbors(viz_window,loc):
+    add_node_to_heap(viz_window,loc,(loc[0]-1,loc[1]),0)
+    add_node_to_heap(viz_window,loc,(loc[0],loc[1]-1),0)
+    add_node_to_heap(viz_window,loc,(loc[0]+1,loc[1]),0)
+    add_node_to_heap(viz_window,loc,(loc[0],loc[1]+1),0)
+    add_node_to_heap(viz_window,loc,(loc[0]-1,loc[1]-1),1)
+    add_node_to_heap(viz_window,loc,(loc[0]+1,loc[1]-1),1)
+    add_node_to_heap(viz_window,loc,(loc[0]-1,loc[1]+1),1)
+    add_node_to_heap(viz_window,loc,(loc[0]+1,loc[1]+1),1)
 
 def process_node(viz_window,node):
-    global node_info
+    global node_info, solved
     dist, loc, parent_loc = node
     if node_info[loc[0],loc[1],0] == 1:
         return
@@ -88,7 +112,13 @@ def process_node(viz_window,node):
     node_info[loc[0],loc[1],1] = parent_loc[0]
     node_info[loc[0],loc[1],2] = parent_loc[1]
     node_info[loc[0],loc[1],3] = dist
-    add_neighbors(viz_window,node)
+    pygame.draw.rect(viz_window,BLUE,(loc[0],loc[1],WIDTH_X,WIDTH_Y))
+    if reached_goal(loc):
+        print("Success")
+        solved = True
+        compute_final_path(viz_window)
+    add_neighbors(viz_window,loc)
+    save_frame(viz_window)
 
 def dijkstra(viz_window):
     global min_heap,solved
@@ -114,8 +144,6 @@ def initialize_start_and_goal_pos(viz_window):
             continue
         start_pos = (start_x,start_y)
         goal_pos = (goal_x,goal_y)
-    print(start_pos)
-    print(goal_pos)
 
 def main():
 
@@ -123,7 +151,7 @@ def main():
 
     # Initializing the grid world as a pygame display window,
     pygame.display.set_caption('Dijkstra Path Finding Algorithm Visualization')
-    viz_window = pygame.display.set_mode(GRID_SIZE)
+    viz_window = pygame.display.set_mode(GRID_SIZE,flags=pygame.HIDDEN)
     viz_window.fill(WHITE)
     save_frame(viz_window)
     initialize_map(viz_window)
@@ -131,6 +159,7 @@ def main():
     save_frame(viz_window)
     #print(in_obstacle(viz_window,(100,100)))
     initialize_start_and_goal_pos(viz_window)
+    dijkstra(viz_window)
 
 if __name__ == "__main__":
     main()
